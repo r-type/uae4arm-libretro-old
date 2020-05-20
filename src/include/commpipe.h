3cd2dc2b158b19880f2044ab34ba7a2173544145
r-type <rtype@email.com>
2020-05-20 12:57:57 +0200

    Revert "fix2"
    This reverts commit 54114dfe59db7a0ed07cdf44173ed726594b5b45.

--- src/include/commpipe.h
+++ src/include/commpipe.h
@@ -48,11 +48,9 @@ STATIC_INLINE void destroy_comm_pipe (smp_comm_pipe *p)
     uae_sem_destroy (&p->lock);
     uae_sem_destroy (&p->reader_wait);
     uae_sem_destroy (&p->writer_wait);
-/*
     p->lock = 0;
     p->reader_wait = 0;
     p->writer_wait = 0;
-*/
     if(p->size > 0 && p->data != NULL)
       free(p->data);
 }
